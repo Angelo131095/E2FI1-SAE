6bd31b6fbe0970b296d82c34775e8ffb2f91b251
Angelo <angelo.ripperger@uni-mannheim.de>
2021-02-26 12:23:04 +0100

    Init Comit

---
+++ PHP/halloWelt.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+
+// Wie immer: Erstes Programm: Hallo Welt!
+/* 
+mehrzeiliger 
+Kommentar
+*/
+
+echo "<h1>Hallo Welt!</h1>";
+
+?>
+
+<p> Hier ist wieder nur HTML</p>
+
+<?php
+
+echo "<strong>und wieder PHP </strong>";
+
+echo "<h2>PHP - Einstellungen:</h2>";
+
+phpinfo();
+
+
+
+
+?>
+
+
+</body>
+</html>
