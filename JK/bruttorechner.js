ff83e2c7fac899379c017eb3e6ecabcf02ac837c
jkol <jkol@camelot-itlab.com>
2021-03-19 11:15:53 +0100

    jk bruttotechner

---
+++ JK/bruttorechner.js
@@ -0,0 +1,32 @@
+
+// let netto = document.getElementsByTagName('netto')
+
+function calcBrutto() {
+    alert('test')
+    console.log('gg');
+    let netto = document.getElementsByName('netto')
+    netto = Number.
+    
+}
+document.addEventListener("DOMContentLoaded", function (event) {
+    let form = document.getElementById("form");
+
+    form.onsubmit = function() {
+        calcBrutto();
+    };
+});
+// * brauchst du nicht
+// calcBrutto();
+// let form = document.getele
+
+
+// form.addEventListener('submit', (event) => {
+//     alert('test')
+//     console.log('gg');
+//     sendData();
+// });
+
+// form.onsubmit = async (e) => {
+//     alert('test')
+//     console.log('gg');
+// };
