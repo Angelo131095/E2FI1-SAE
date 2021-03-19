010a37fe71ecd1ff997a83a9782f1723ec0288ae
Jim Knopf <jimknopf8jk@gmail.com>
2021-03-19 12:08:02 +0100

    cleanup

--- JK/bruttorechner.js
+++ JK/bruttorechner.js
@@ -1,14 +1,4 @@
 
-// let netto = document.getElementsByTagName('netto')
-document.addEventListener("DOMContentLoaded", function (event) {
-    let form = document.getElementById("form");
-
-    form.onsubmit = function(e) {
-        e.preventDefault();
-        calcBrutto();
-    };
-});
-
 function calcBrutto(e) {
     let netto = Number.parseInt(document.getElementById('netto').value)
     let percent = Number.parseInt(document.getElementById('percent').value)
